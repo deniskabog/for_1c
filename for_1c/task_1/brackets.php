7251a75696645035e075e0a57e3fe11550b265ea
deniskabog <82509953+deniskabog@users.noreply.github.com>
2022-02-16 15:31:55 +0300

    Update brackets.php

--- for_1c/task_1/brackets.php
+++ for_1c/task_1/brackets.php
@@ -1,13 +1,16 @@
 <?php
 $column = 0;
 $string = '2 + (4 - 3)';
+
 for ($i=0; $i<strlen($string); $i++) {
     $char = $string[$i];
     if ($char == '(')
         $column++;
     if ($char == ')')
         $column--;
+    if ($column == -1)
+        break;
 }
 if ($column == 0) {
     echo 'Количество скобок совпадает';
-} else echo 'Ошибка';
+} else echo 'Ошибка в написании скобок';
